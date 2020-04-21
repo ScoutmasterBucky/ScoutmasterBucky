73634fe0c521e155c359f11dd7d3538b96400ab5
Tyler Akins <fidian@rumkin.com>
2020-04-20 21:54:02 -0500

    Fixing page breaks for wkhtmltopdf

--- helpers/wb-note.js
+++ helpers/wb-note.js
@@ -1,3 +1,3 @@
 module.exports = function (obj) {
-    return `<div class="Tt(u) My(0.25em) Bgc(smbAccentGold) Bdc(red) Bdw(0.25em) Bdw(0.25em)--s P(1em) Bi(av) P(0.5em)--s">${obj.fn(this)}</div>`;
+    return `<div class="Tt(u) Mt(1em) Bgc(smbAccentGold) Bdc(red) Bdw(0.25em) Bdw(0.25em)--s P(1em) Bi(av) Pbi(av) P(0.5em)--s">${obj.fn(this)}</div>`;
 };
--- helpers/wb-text-entry.js
+++ helpers/wb-text-entry.js
@@ -3,7 +3,7 @@ let textAreaNum = 0;
 module.exports = function (obj) {
     var height = obj.hash.lines * (22 * 1.2); // 22px font size, 1.2em line height
     var content = obj.fn(this);
-    var result = '<div class="P(0.25em) Bdstartw(1pt) Bdendw(1pt) Bdbw(1pt) Bi(av) Lh(1.2em)">';
+    var result = '<div class="P(0.25em) Bdstartw(1pt) Bdendw(1pt) Bdbw(1pt) Bi(av) Pbi(av) Lh(1.2em)">';
 
     if (content.trim().length) {
         result += `<div class="Mb(0.25em)">${content}</div>`;
