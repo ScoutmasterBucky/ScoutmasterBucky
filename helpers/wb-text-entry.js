ecb3e2b21c0eb084b87c83dbfa5d92d0b78fdfcb
Tyler Akins <fidian@rumkin.com>
2020-04-23 11:07:52 -0500

    Making sure names are set for form fields

--- helpers/wb-text-entry.js
+++ helpers/wb-text-entry.js
@@ -9,6 +9,7 @@ module.exports = function (obj) {
         result += `<div class="Mb(0.25em)">${content}</div>`;
     }
 
+    // Add a unique name for each form field so wkhtmltopdf picks up the form field
     result += `<textarea name="ta${textAreaNum++}" class="Fz(22px) Bgc(smbAccentGold):h W(100%) Bdw(0) Mih(${height}px) Rsz(v)"></textarea></div>`;
 
     return result;
