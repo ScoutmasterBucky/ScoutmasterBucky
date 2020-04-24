6b05cc3c971fcde518be5529687154e99c252679
Tyler Akins <fidian@rumkin.com>
2020-04-23 20:07:28 -0500

    Bucky adjustments

--- helpers/wb-alert.js
+++ helpers/wb-alert.js
@@ -1,3 +1,3 @@
 module.exports = function (obj) {
-    return `<div class="Tt(u) Mt(1em) Bgc(smbAccentGold) Bdc(red) Bdw(0.25em) P(1em) Bi(av) Pbi(av) P(0.5em)--s">${obj.fn(this)}</div>`;
+    return `<div class="Tt(u) Mt(1em) Bgc(smbAccentGold) Bdc(red) Bdw(0.25em) P(1em) Bi(av) Pbi(av) P(0.5em)--s Fw(b) C(red)">${obj.fn(this)}</div>`;
 };
