8e14ca344ce00db2a329f55f78db177ab6d73ccd
Tyler Akins <fidian@rumkin.com>
2021-12-02 17:34:57 -0600

    Correcting time zone alteration

--- handlebars/pages/helpers/date.js
+++ handlebars/pages/helpers/date.js
@@ -3,7 +3,7 @@ const luxon = require('luxon');
 module.exports = function (obj) {
     const format = obj.hash.format || 'MMMM d, yyyy';
     const dateStr = obj.fn(this);
-    const d = luxon.DateTime.fromFormat(dateStr, 'yyyy-MM-dd H:mm', { zome: 'America/Chicago', setZone: true })
+    const d = luxon.DateTime.fromFormat(dateStr, 'yyyy-MM-dd H:mm', { zone: 'America/Chicago' })
 
     if (format === 'iso') {
         return d.setZone('utc').toISO();
