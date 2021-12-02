8d5add29c507bb25a2eb59edccb0a484833f350a
Tyler Akins <fidian@rumkin.com>
2021-12-02 12:16:07 -0600

    Fixing UTC offsets

--- handlebars/pages/helpers/date.js
+++ handlebars/pages/helpers/date.js
@@ -3,10 +3,10 @@ const luxon = require('luxon');
 module.exports = function (obj) {
     const format = obj.hash.format || 'MMMM d, yyyy';
     const dateStr = obj.fn(this);
-    const d = luxon.DateTime.fromFormat(dateStr, 'yyyy-MM-dd H:mm', { zome: 'America/Chicago' })
+    const d = luxon.DateTime.fromFormat(dateStr, 'yyyy-MM-dd H:mm', { zome: 'America/Chicago', setZone: true })
 
     if (format === 'iso') {
-        return d.toISO();
+        return d.setZone('utc').toISO();
     }
 
     return d.toFormat(format)
