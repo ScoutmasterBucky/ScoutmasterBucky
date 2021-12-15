a105903e056684f1a10f531bbc42c4440d041bc0
Tyler Akins <fidian@rumkin.com>
2021-12-14 18:00:59 -0600

    Support ISO dates so the Last Update on index page works

--- handlebars/pages/helpers/date.js
+++ handlebars/pages/helpers/date.js
@@ -3,7 +3,14 @@ const luxon = require('luxon');
 module.exports = function (obj) {
     const format = obj.hash.format || 'MMMM d, yyyy';
     const dateStr = obj.fn(this);
-    const d = luxon.DateTime.fromFormat(dateStr, 'yyyy-MM-dd H:mm', { zone: 'America/Chicago' })
+
+    let d;
+
+    if (dateStr.match(/^\d\d\d\d-\d\d-\d\d \d\d?:\d\d$/)) {
+        d = luxon.DateTime.fromFormat(dateStr, 'yyyy-MM-dd H:mm', { zone: 'America/Chicago' })
+    } else {
+        d = luxon.DateTime.fromISO(dateStr, { zone: 'America/Chicago' });
+    }
 
     if (format === 'iso') {
         return d.setZone('utc').toISO();
