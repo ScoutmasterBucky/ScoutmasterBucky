4288f9ab9f436f4407a103efb71bba834d50aa8d
Tyler Akins <fidian@rumkin.com>
2022-11-27 10:08:54 -0600

    Improving last update times for assets

--- handlebars/pages/helpers/date.js
+++ handlebars/pages/helpers/date.js
@@ -1,20 +1,26 @@
-const luxon = require('luxon');
+const luxon = require("luxon");
 
 module.exports = function (obj) {
-    const format = obj.hash.format || 'MMMM d, yyyy';
+    const format = obj.hash.format || "MMMM d, yyyy";
     const dateStr = obj.fn(this).trim();
 
     let d;
 
     if (dateStr.match(/^\d\d\d\d-\d\d-\d\d \d\d?:\d\d$/)) {
-        d = luxon.DateTime.fromFormat(dateStr, 'yyyy-MM-dd H:mm', { zone: 'America/Chicago' })
+        d = luxon.DateTime.fromFormat(dateStr, "yyyy-MM-dd H:mm", {
+            zone: "America/Chicago"
+        });
+    } else if (dateStr.match(/^\d+$/)) {
+        d = new Date();
+        d.setTime(+dateStr);
+        d = luxon.DateTime.fromJSDate(d);
     } else {
-        d = luxon.DateTime.fromISO(dateStr, { zone: 'America/Chicago' });
+        d = luxon.DateTime.fromISO(dateStr, { zone: "America/Chicago" });
     }
 
-    if (format === 'iso') {
-        return d.setZone('utc').toISO();
+    if (format === "iso") {
+        return d.setZone("utc").toISO();
     }
 
-    return d.toFormat(format)
+    return d.toFormat(format);
 };
