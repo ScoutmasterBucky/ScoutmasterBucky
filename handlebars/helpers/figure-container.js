61a48bb3b99c2ba0f5c3f1fb28c781b8a182c2f1
Tyler Akins <fidian@rumkin.com>
2024-02-29 20:26:06 -0600

    Adding class prep placeholder pages

--- handlebars/helpers/figure-container.js
+++ handlebars/helpers/figure-container.js
@@ -2,12 +2,12 @@ module.exports = function (obj) {
     const figureContent = obj.fn(this);
     const normalContent = obj.inverse(this);
     const alignLeft = obj.hash.align === "left";
-    const align = alignLeft ? "Fl(start)--_s" : "Fl(end)--_s";
+    const align = alignLeft ? "Fxd(r)--_s" : "Fxd(rre)--_s";
     const positionTop = obj.hash.position === "top";
     const position = positionTop ? "Fxd(c)--s" : "Fxd(cre)--s";
 
-    return `<div class="D(f)--s Ai(c)--s ${position}">
-    <div class="D(f) Fxd(c) ${align} Fxs(0) W(20%)--_sml W(25%)--l W(33%)--m W(50%)--s Px(0.8em) Py(0.4em) Ai(c)">
+    return `<div class="D(f) Ai(c)--s ${position} ${align}">
+    <div class="D(f) Fxd(c) Fxs(0) W(20%)--_sml W(25%)--l W(33%)--m W(50%)--s Px(0.8em) Py(0.4em) Ai(c)">
         ${figureContent}
     </div>
     <div>
