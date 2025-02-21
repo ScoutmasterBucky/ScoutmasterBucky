5b1622951761847e0cefdaefb2b22b9e37d2ecc5
Tyler Akins <fidian@rumkin.com>
2025-02-20 18:09:38 -0600

    Adding Scouts BSA novas

---
+++ src/pages/nova-lab/scouts-bsa/shoot/index.vue
@@ -0,0 +1,23 @@
+<page>
+title: Shoot! Nova Award
+layout: nova
+nova: shoot
+program: scouts-bsa
+</page>
+
+<script setup lang="ts">
+import requirements from '../../../../../data/nova-lab/scouts-bsa/shoot.yaml';
+</script>
+
+<template>
+    <NovaPage
+        :nova="$frontmatter.nova"
+        :program="$frontmatter.program"
+        :requirements="requirements"
+    >
+        <p>
+            This module is designed for Scouts BSA to explore how projectiles
+            and space affect your life each day.
+        </p>
+    </NovaPage>
+</template>
