4f79df228b92727191e43b530cc9fde0c29ea152
Tyler Akins <fidian@rumkin.com>
2025-02-23 13:43:57 -0600

    Trying to set the execution policy

--- start-server.bat
+++ start-server.bat
@@ -1,2 +1,10 @@
 @echo off
+echo "Setting execution policy - press enter to continue"
+pause
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+echo "I hope it worked"
+echo "Starting server - press enter to continue"
+pause
 npm run start
+echo "I hope that worked too"
+pause
