dc7e4c367ec848bf0bdce610b4df189a641d075c
Tyler Akins <fidian@rumkin.com>
2025-03-08 08:13:57 -0600

    Changing footer to not use an image

--- src/components/SmbFooter.vue
+++ src/components/SmbFooter.vue
@@ -1,9 +1,43 @@
+<script setup lang="ts">
+const year = new Date().getFullYear();
+</script>
+
 <template>
     <footer class="unprintable">
-        <img
-            src="/images/footer.jpg"
-            alt="Developing today's youth to become tomorrow's leaders"
-            class="wide"
-        />
+        <div class="saying">Developing today's youth to become tomorrow's leaders</div>
+        <div class="bar">© {{year}} Brian Reiners (Scoutmaster Bucky) &middot; All Rights Reserved</div>
     </footer>
 </template>
+
+<style scoped>
+.saying {
+    padding: 0.5em;
+    text-align: center;
+    font-size: 2em;
+    font-weight: bold;
+    color: var(--footer-saying-color);
+}
+
+@media (min-width: 768.0001px) and (max-width: 1024px) {
+    .saying {
+        font-size: 1.5em;
+    }
+}
+
+@media (max-width: 768px) {
+    .saying {
+        font-size: 1em;
+    }
+}
+
+.bar {
+    text-align: center;
+    font-size: 0.8em;
+    color: var(--footer-bar-color);
+    border-top: var(--footer-bar-border);
+    border-bottom: var(--footer-bar-border);
+    background: var(--footer-bar-background);
+    padding: 6px 10px;
+    text-transform: uppercase;
+}
+</style>
