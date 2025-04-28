3088710f56205749ca5762866234fc6da14b8340
Tyler Akins <fidian@rumkin.com>
2025-04-28 11:10:32 -0500

    Various updates
    Downloaded new requirement versions
    
    Updated aquatics guide and related other awards
    
    Linked merit badges to other awards
    
    Fixed formatting for other awards and linked to merit badges

--- src/pages/other-awards/complete-angler/index.vue
+++ src/pages/other-awards/complete-angler/index.vue
@@ -13,7 +13,7 @@ import requirements from '~/data/other-awards/complete-angler.yaml';
 
     <h3>Complete Angler</h3>
 
-    <p>The Scouting America Complete Angler Award honors Scouts who earn all three fishing-related merit badges: Fishing, Fly-Fishing, and Fish & Wildlife Management.</p>
+    <p>The Scouting America Complete Angler Award honors Scouts who earn all three fishing-related merit badges: Fishing, Fly-Fishing, and Fish &amp; Wildlife Management.</p>
 
     <p>Fishing Merit Badge was one of the original merit badges and over two million have earned it. In a recent Boys’ Life survey, fishing placed 4th overall as the preferred outdoor activity, surpassed only by camping, swimming and bicycling. What better way to keep older Scouts involved than by expanding on this trend.</p>
 
@@ -21,7 +21,7 @@ import requirements from '~/data/other-awards/complete-angler.yaml';
 
     <p><i>The Complete Angler patch is available as a patch which may be worn as a temproary patch on the right pocket of any Scouting uniform.</i></p>
 
-    <p><a href="/other-awards/complete-angler-recognition.pdf">Complete Angler Award Application</a></p>
+    <p><a href="/other-awards/complete-angler.pdf">Complete Angler Award Application</a></p>
 
     <h2>Complete Angler Requirements</h2>
 
