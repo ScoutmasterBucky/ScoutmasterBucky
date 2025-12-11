28a4853ecc6ff5ad6b49c5f115f8686c2174e787
Tyler Akins <fidian@rumkin.com>
2025-12-11 12:32:24 -0600

    Improving 404 handling

---
+++ src/assets/404-setup.ts
@@ -0,0 +1,66 @@
+import { component, css, html, rootElement } from 'fudgel';
+import redirects from '~/data/redirects.yaml';
+import globToRegExp from 'glob-to-regexp';
+
+interface RedirectSuggestion {
+    match: string | RegExp;
+    url: string;
+    title: string;
+    description?: string;
+    date?: Date;
+}
+
+component(
+    'offer-suggestions',
+    {
+        template: html`
+            <p>
+                The file you were attempting to reach no longer is at this
+                location. From time to time, we move files around to improve our
+                site structure, for updated content, or to remove outdated
+                information.
+            </p>
+
+            <div *if="suggestions?.length">
+                <p>
+                    Here are some suggestions that might help you find what you
+                    are looking for:
+                </p>
+                <ul>
+                    <li *for="suggestion of suggestions">
+                        <a href="{{suggestion.url}}">{{ suggestion.title }}</a>
+                        <span *if="suggestion.description">
+                            - {{ suggestion.description }}</span
+                        >
+                    </li>
+                </ul>
+            </div>
+
+            <p>
+                Please use the navigation menu at the top of the page to find
+                what you are looking for, or return to the
+                <a href="/">home page</a>.
+            </p>
+        `,
+    },
+    class {
+        suggestions: [];
+
+        onInit() {
+            const urlFull = window.location
+                .toString()
+                .replace(
+                    `${window.location.protocol}//${window.location.host}`,
+                    ''
+                );
+            const urlPath = window.location.pathname;
+            this.suggestions = redirects.filter(
+                (redirect: RedirectSuggestion) => {
+                    const regexp = globToRegExp(redirect.match);
+
+                    return regexp.test(urlFull) || regexp.test(urlPath);
+                }
+            );
+        }
+    }
+);
